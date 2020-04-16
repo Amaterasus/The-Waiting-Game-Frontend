5e4c87bd4106416ade4a44e1f8b24e382d01432d
Amaterasus <skyetay03@googlemail.com>
2020-04-16 16:19:32 +0100

    multiple quizes

--- src/game.js
+++ src/game.js
@@ -1,116 +1,155 @@
-
-    const runGame = () => {
-        fetch(QUIZ_URL)
-        .then(res => res.json())
-        .then(response => quizMaster(response.results))
-        flip = true 
-    }
-
-
-    async function quizMaster(results) {
-        let questions = [...results]
-        const score = document.createElement("h2")
-        score.className = "col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-right"
-        score.innerText = "Score: "
-        let scoreSpan = document.createElement("span")
-        scoreSpan.innerText = 0
-        score.append(scoreSpan)
-
-        while (questions.length > 0)
+async function pickGame() {
+    main.innerText = ""
+    const games = [
         {
-            main.innerText= ""
-            main.append(score)
-            // pull out a random question
-            const question = questions.sort(() => Math.random() - 0.5).pop(); // this works
-            // pull out the answer of the question
-            const answer = question.correct_answer // this works
-            // let the user decide between the answer and the incorrect
-            const choices = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5); // this works
-            
-            // go to another fucntion where you render the question
-            // then you return the users answer
-            
-            const userAnswer = await renderQuestion(question.question, choices) // make a function here that renders the page and returns the users answer
-            
-            main.innerText= ""
-            main.append(score)
-
-            userAnswer === answer ? scoreSpan.innerText++ : false
-
-            const con = await renderAnswers(question.question, choices, answer)
-
-             // if true increase the points by 1 else do nothing
-            // compare the users choice with the answer we pulled out at the start
-
+            number: 9,
+            quiz: "General Knowledge"
+        },
+        {
+            number: 31,
+            quiz: "Japanese Anime & Manga"
         }
+    ]
+    const number = await renderQuizBoxes(games)
+    console.log(number)
 
+    runGame(number)
+}
+
+const renderQuizBoxes = (games) => {
+    const div = document.createElement('div')
+    games.forEach(game => {
+        const choiceTag = document.createElement("button")
+        choiceTag.className = "btn btn-primary"
+        choiceTag.innerHTML = game.quiz 
+        choiceTag.value = game.number
+
+        div.append(choiceTag)
+    })
+
+    main.append(div)
+
+    return new Promise((resolve) => {
+        div.addEventListener('click', (e) => {
+            if (e.target.tagName === "BUTTON") resolve(e.target.value)
+        })
+
+    })
+}
+
+const runGame = (quizNumber) => {
+    console.log("I'm in run game!")
+    fetch(`${QUIZ_START_URL}${quizNumber}${QUIZ_END_URL}`)
+    .then(res => res.json())
+    .then(response => quizMaster(response.results))
+    flip = true 
+}
+
+
+async function quizMaster(results) {
+    let questions = [...results]
+    const score = document.createElement("h2")
+    score.className = "col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-right"
+    score.innerText = "Score: "
+    let scoreSpan = document.createElement("span")
+    scoreSpan.innerText = 0
+    score.append(scoreSpan)
+
+    while (questions.length > 0)
+    {
+        main.innerText= ""
+        main.append(score)
+        // pull out a random question
+        const question = questions.sort(() => Math.random() - 0.5).pop(); // this works
+        // pull out the answer of the question
+        const answer = question.correct_answer // this works
+        // let the user decide between the answer and the incorrect
+        const choices = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5); // this works
         
+        // go to another fucntion where you render the question
+        // then you return the users answer
+        
+        const userAnswer = await renderQuestion(question.question, choices) // make a function here that renders the page and returns the users answer
+        
+        main.innerText= ""
+        main.append(score)
+
+        userAnswer === answer ? scoreSpan.innerText++ : false
+
+        await renderAnswers(question.question, choices, answer)
+
+            // if true increase the points by 1 else do nothing
+        // compare the users choice with the answer we pulled out at the start
+
     }
 
-    const renderQuestion = (question, choices) => {
-        
-        const div = document.createElement('div')
-        div.className = "offset-xl-2 offset-lg-2 offset-md-2 col-xl-8 col-lg-8 col-md-8 col-sm-8 col-xs-8 text-center"
-        const questionTag = document.createElement("h2")
-        questionTag.innerHTML = question
+    
+}
 
-        div.append(questionTag)
-        
-        choices.forEach( choice => {
-            const choiceTag = document.createElement("button")
-            // choiceTag.append
-            choiceTag.className = "btn btn-primary"
-            choiceTag.innerHTML = choice 
-            choiceTag.value = choice 
+const renderQuestion = (question, choices) => {
+    
+    const div = document.createElement('div')
+    div.className = "offset-xl-2 offset-lg-2 offset-md-2 col-xl-8 col-lg-8 col-md-8 col-sm-8 col-xs-8 text-center"
+    const questionTag = document.createElement("h2")
+    questionTag.innerHTML = question
 
-            div.append(choiceTag)
-        
+    div.append(questionTag)
+    
+    choices.forEach( choice => {
+        const choiceTag = document.createElement("button")
+        // choiceTag.append
+        choiceTag.className = "btn btn-primary"
+        choiceTag.innerHTML = choice 
+        choiceTag.value = choice 
+
+        div.append(choiceTag)
+    
+    })
+
+    main.append(div)
+
+    return new Promise((resolve) => {
+        div.addEventListener('click', (e) => {
+            if (e.target.tagName === "BUTTON") resolve(e.target.value)
         })
+    })
+}
 
-        main.append(div)
+const renderAnswers = (question, choices, answer) => {
+    
+    const div = document.createElement('div')
+    div.className = "offset-xl-2 offset-lg-2 offset-md-2 col-xl-8 col-lg-8 col-md-8 col-sm-8 col-xs-8 text-center"
+    const questionTag = document.createElement("h2")
+    questionTag.innerHTML = question
 
-        return new Promise((resolve) => {
-            div.addEventListener('click', (e) => {
-                if (e.target.tagName === "BUTTON") resolve(e.target.value)
-            })
-        })
-    }
+    div.append(questionTag)
+    console.log(answer)
 
-    const renderAnswers = (question, choices, answer) => {
+    choices.forEach( choice => {
+        console.log(choice)
+        const choiceTag = document.createElement("button")
+        // choiceTag.append
+        if (choice === answer)
+        {
+            choiceTag.className = "btn btn-success"
+        }
+        else
+        {
+            choiceTag.className = "btn btn-danger"
+        }
         
-        const div = document.createElement('div')
-        div.className = "offset-xl-2 offset-lg-2 offset-md-2 col-xl-8 col-lg-8 col-md-8 col-sm-8 col-xs-8 text-center"
-        const questionTag = document.createElement("h2")
-        questionTag.innerHTML = question
+        choiceTag.innerHTML = choice 
+        choiceTag.value = choice 
 
-        div.append(questionTag)
-        console.log(answer)
+        div.append(choiceTag)
+    
+    })
 
-        choices.forEach( choice => {
-            console.log(choice)
-            const choiceTag = document.createElement("button")
-            // choiceTag.append
-            if (choice === answer)
-            {
-                choiceTag.className = "btn btn-success"
-            }
-            else
-            {
-                choiceTag.className = "btn btn-danger"
-            }
-            
-            choiceTag.innerHTML = choice 
-            choiceTag.value = choice 
+    main.append(div)
 
-            div.append(choiceTag)
-        
+    return new Promise((resolve) => {
+        div.addEventListener('click', (e) => {
+            resolve(e.target.value)
         })
-
-        main.append(div)
-
-        return new Promise((resolve) => {
-            div.addEventListener('click', (e) => {
-                resolve(e.target.value)
-            })
-        })
-    }
+    })
+}
--- src/login.js
+++ src/login.js
@@ -1,8 +1,10 @@
 // API
 const BASE_URL = "https://twgbackend.herokuapp.com"
 const USER_URL = `${BASE_URL}/users`
-const QUIZ_URL = "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple"
+const QUIZ_START_URL = "https://opentdb.com/api.php?amount=20&category="
+const QUIZ_END_URL = "&difficulty=easy"
 const DRINKS_URL = `${BASE_URL}/drinks`
+
 
 //current user 
 let current_user = false
@@ -38,105 +40,104 @@ let itemArray = [];
 let flip = true
 
 
-    getDrinks.addEventListener("click", e => {
-        e.preventDefault()
-        getDrinks.classList.add("active")
-        game.classList.remove("active")
+getDrinks.addEventListener("click", e => {
+    e.preventDefault()
+    getDrinks.classList.add("active")
+    game.classList.remove("active")
 
-    if (!current_user === true ){
-        loginPage()
-        getDrinks.classList.add("disabled")
-    }
-    else {
-        fetch(DRINKS_URL).then(res => res.json()).then(drinks => {
-            allDrinks = drinks;
-          renderDrinks(drinks)
-        })
-    }
-    
-  })
-
-    game.addEventListener("click", e => {
-        e.preventDefault()
-        getDrinks.classList.remove("disabled")
-        getDrinks.classList.remove("active")
-        game.classList.add("active")
-        getDrinks.classList.remove("disabled")
-        runGame()
-        
+if (!current_user === true ){
+    loginPage()
+    getDrinks.classList.add("disabled")
+}
+else {
+    fetch(DRINKS_URL).then(res => res.json()).then(drinks => {
+        allDrinks = drinks;
+        renderDrinks(drinks)
     })
+}
+
+})
+
+game.addEventListener("click", e => {
+    e.preventDefault()
+    getDrinks.classList.remove("disabled")
+    getDrinks.classList.remove("active")
+    game.classList.add("active")
+    getDrinks.classList.remove("disabled")
+    pickGame()
     
-    const loginPage = () => {
-        main.innerText = ""
-        main.className = "container-fluid"
-        // const anchorDiv = document.createElement("div")
-        //  anchorDiv.className = "container h-100"
-        const parentDiv = document.createElement("div")
-        parentDiv.className = "offset-lg-3 col-lg-6 row h-100 text-center justify-content-center align-items-center parent"
-        const childDiv = document.createElement("div")
-        childDiv.className = " col-4 colour"
-        const form = document.createElement("form")
-        const pButton = document.createElement("p")
-        const inputButton = document.createElement("input")
-        inputButton.className = "btn btn-success"
-        inputButton.type = "submit"
-        inputButton.value = "Submit"
-        const label1 = document.createElement("label")
-        const p1 = document.createElement("p")
-        label1.innerText = "Name:  "
-        const inputName = document.createElement("input")
-        inputName.name = "username"
-        inputName.placeholder = "Enter name..."
-        const p2 = document.createElement("p")
-        const label2 = document.createElement("label")
-        label2.innerText = "Table Number:   "
-        const inputTable = document.createElement("input")
-        inputTable.placeholder = "Enter table number..."
-        inputTable.name = "tablenumber"
+})
 
-        pButton.append(inputButton)
-        p1.append(label1, inputName)
-        p2.append(label2, inputTable)
-        form.append(p1,p2, pButton)
-        childDiv.append(form)
-        parentDiv.append(childDiv)
-        // anchorDiv.append(parentDiv)
-        main.append(parentDiv)
+const loginPage = () => {
+    main.innerText = ""
+    main.className = "container-fluid"
+    // const anchorDiv = document.createElement("div")
+    //  anchorDiv.className = "container h-100"
+    const parentDiv = document.createElement("div")
+    parentDiv.className = "offset-lg-3 col-lg-6 row h-100 text-center justify-content-center align-items-center parent"
+    const childDiv = document.createElement("div")
+    childDiv.className = " col-4 colour"
+    const form = document.createElement("form")
+    const pButton = document.createElement("p")
+    const inputButton = document.createElement("input")
+    inputButton.className = "btn btn-success"
+    inputButton.type = "submit"
+    inputButton.value = "Submit"
+    const label1 = document.createElement("label")
+    const p1 = document.createElement("p")
+    label1.innerText = "Name:  "
+    const inputName = document.createElement("input")
+    inputName.name = "username"
+    inputName.placeholder = "Enter name..."
+    const p2 = document.createElement("p")
+    const label2 = document.createElement("label")
+    label2.innerText = "Table Number:   "
+    const inputTable = document.createElement("input")
+    inputTable.placeholder = "Enter table number..."
+    inputTable.name = "tablenumber"
 
-        form.addEventListener("submit", event=> {
-            event.preventDefault()
-            const name = event.target.username.value
-            // current_user = event.target.username.value
-            const number = event.target.tablenumber.value
-            currentUser.name.push(name)
-            currentUser.table.push(number)
-            createUser(name,number)
-        })
+    pButton.append(inputButton)
+    p1.append(label1, inputName)
+    p2.append(label2, inputTable)
+    form.append(p1,p2, pButton)
+    childDiv.append(form)
+    parentDiv.append(childDiv)
+    // anchorDiv.append(parentDiv)
+    main.append(parentDiv)
+
+    form.addEventListener("submit", event=> {
+        event.preventDefault()
+        const name = event.target.username.value
+        // current_user = event.target.username.value
+        const number = event.target.tablenumber.value
+        currentUser.name.push(name)
+        currentUser.table.push(number)
+        createUser(name,number)
+    })
+}
+
+const createUser = (name, number) => {
+        fetch(USER_URL, {
+            method: "POST",
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+            name: name,
+            table_number: number
+            })
+        }).then(parseJSON)
+    .catch(error => error.then(msg => {alert(msg.errors)}));
+};
+
+const parseJSON = resp => {
+    console.log("this is the response from the server", resp);
+    if (resp.ok) {
+        return resp.json().then(fetch(DRINKS_URL).then(res => res.json()).then(drinks => {
+            allDrinks = drinks;
+            renderDrinks(drinks)
+        }))
+    } else {
+        throw resp.json();
     }
-
-    const createUser = (name, number) => {
-         fetch(USER_URL, {
-             method: "POST",
-             headers: {
-                'Content-Type': 'application/json'
-             },
-             body: JSON.stringify({
-                name: name,
-                table_number: number
-             })
-         }).then(parseJSON)
-        .catch(error => error.then(msg => {alert(msg.errors)}));
     };
-
-    const parseJSON = resp => {
-        console.log("this is the response from the server", resp);
-        if (resp.ok) {
-            return resp.json().then(fetch(DRINKS_URL).then(res => res.json()).then(drinks => {
-                allDrinks = drinks;
-              renderDrinks(drinks)
-            }))
-        } else {
-          throw resp.json();
-        }
-      };
-    
